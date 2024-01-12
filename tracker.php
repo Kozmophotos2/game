338448fd984f78c58d818e7167477c5eecc372a6
Kozmophotos2 <156229422+Kozmophotos2@users.noreply.github.com>
2024-01-12 10:28:22 +0000

    Update tracker.php

--- tracker.php
+++ tracker.php
@@ -1,13 +1,1 @@
-<?php
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $date = date("Y-m-d H:i:s");
-    $logfile = 'iplog.txt';
 
-    // Log the IP address and date/time
-    $logEntry = "IP: $ip - Date/Time: $date\n";
-    file_put_contents($logfile, $logEntry, FILE_APPEND);
-
-    // Display the logged IP addresses
-    $logContent = file_get_contents($logfile);
-    echo "<h2>IP Addresses Logged:</h2><pre>$logContent</pre>";
-?>
